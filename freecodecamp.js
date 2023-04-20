552ff2b7eeaf7101dd7e5d9f01362c2777b4f16b
Joshua Ramos <joshua.m.ramos32@gmail.com>
2023-04-20 08:28:54 -0500

    just running through basics

---
+++ freecodecamp.js
@@ -0,0 +1,41 @@
+//This is an inloine comment//
+/* This is a
+multi line comment
+ */
+// var myName;
+// var a;
+// var a = 7;
+// var b;
+// b = a;
+// var a = 9;
+// var myFirstName = "Josh";
+// var myLastName = "Ramos";
+// var a = 5;
+// var b = 10;
+// var c = "I am a !";
+// let catName = "Oliver";
+// let catSound = "Meow!";
+// console.log(catName)
+// console.log(catSound)
+// catName = "Garfield";
+// console.log(catName);
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// fact = "is awesome!";
+// console.log(FCC, fact);
+// const sum = 10 + 10;
+// console.log(sum);
+// const difference = 45 - 33;
+// console.log(difference);
+// const product = 8 * 10;
+// console.log(product);
+// const quotient = 66 / 33;
+// console.log(quotient);
+// let myDecimal = 5.7;
+const product = 2.0 * 2.5;
+console.log(product);
+const quotient = 4.4 / 2.0;
+console.log(quotient);
+const remainder = 11 % 3;
+console.log(remainder);
+
